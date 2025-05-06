f1732040cf4a56c4e637c0bba24ac1e9a6c9ede8
Esteban Gonzalez <118945397+Teban716@users.noreply.github.com>
2025-05-05 21:47:05 -0500

    Add files via upload

---
+++ Cajero automatico.py
@@ -0,0 +1,45 @@
+banco=[]
+def menu():
+    while True:
+        print('-----------------------------------------------')
+        print("MENU")
+        print("1. Depositar")
+        print("2. Retirar")
+        print("3. Saldo")
+        print("4. Salir")
+        print('-----------------------------------------------')
+        opcion = pedir_numero_positivo('Elige lo que deseas realizar:    ')
+        print('-----------------------------------------------')
+        if opcion == 1:
+            depositar()
+        elif opcion == 2: 
+            retirar()
+        elif opcion == 3:
+            saldo()
+        elif opcion == 4:
+            break
+        else:
+            print('Numero Invalido')
+
+def pedir_numero_positivo(prompt):
+    while(True):
+        try:
+            numero = float(input(prompt))
+            if numero > 0:
+                return numero
+            else:
+                print("El numero no es positivo")
+        except ValueError:
+            print("El numero es invalido")
+
+def depositar():
+    depositar=pedir_numero_positivo
+    banco.append(depositar)
+
+def retirar():
+    print()
+
+def saldo():
+    print()
+
+menu()
