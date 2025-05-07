2a32b5200d574148ef5880989bbfeb4281e8b219
Esteban Gonzalez <118945397+Teban716@users.noreply.github.com>
2025-05-06 21:28:22 -0500

    Add files via upload

--- Cajero automatico.py
+++ Cajero automatico.py
@@ -33,17 +33,24 @@ def pedir_numero_positivo(prompt):
             print("El numero es invalido")
 
 def depositar():
-    depositar = pedir_numero_positivo("Ingrese su valor")
+    depositar = pedir_numero_positivo("Ingrese su valor a depositar;    $")
     banco.append(depositar)
     suma = sum(banco)
-    print(suma)
+    print("\nDEPOSITO EXITOSO")
+    print(f"Se ha depositado ${suma}")
 
 def retirar():
-    retirar=pedir_numero_positivo()
+    if banco == 0:
+        print("FONDOS INSUFICIENTE")
+    retirar = pedir_numero_positivo("Cuanto vas a retirar:    $")
+    if retirar > banco[0]:
+        print("FONDOS INSUFICIENTE")
+        menu()
     retirar_negativo = retirar * (-1)
-    
-    
-    print()
+    banco.append(retirar_negativo)
+    suma = sum(banco)
+    print("\n RETIRO EXITOSO")
+    print(f"Se ha retirado ${suma}")
 
 def saldo():
     print(banco)
